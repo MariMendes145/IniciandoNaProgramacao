54424aceb5c84bf62f02acd378d21828f98f03d9
marimendes145 <98403577+MariMendes145@users.noreply.github.com>
2022-02-10 22:45:20 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+botao = document.getElementById("botaoEnviar");
+botao.addEventListener('click', validaFormulario);
+
+function validaFormulario(){
+  if(document.getElementById("nome").value != "" && document.getElementById("email").value !="" && document.getElementById("telefone").value != ""){
+  alert("Prontinho!Você receberá as novidades por email!")
+  }else{
+  alert("Por favor, preencha os campos nome, email e telefone!")
+  }
+}
